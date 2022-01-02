3ef25e8a472d057df7ba897b68bc4608a263eccc
leegayeong <leegy0419@gmail.com>
2022-01-02 17:46:39 +0900

    SecurityConfig 클래스 생성 #9
    기본적인 Security 설정을 위한 클래스 생성

---
+++ src/main/java/com/cocobob/server/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.cocobob.server.config;
+
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
