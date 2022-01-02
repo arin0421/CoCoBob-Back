00f63c385758f05e4846f83c0b30af2b079908cb
leegayeong <leegy0419@gmail.com>
2022-01-02 18:36:53 +0900

    SecurityConfig 클래스에 의존성 주입 #9
    TokenProvider, JwtAccessDeniedHandler, JwtAccessDeniedHandler 의존성 주입

--- src/main/java/com/cocobob/server/config/SecurityConfig.java
+++ src/main/java/com/cocobob/server/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.cocobob.server.config;
 
+import com.cocobob.server.jwt.JwtAccessDeniedHandler;
+import com.cocobob.server.jwt.JwtAuthenticationEntryPoint;
+import com.cocobob.server.jwt.TokenProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,6 +12,20 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
     @Override
     public void configure(WebSecurity web) {
         web.ignoring()
