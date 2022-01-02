3fdc4b22c5db58600f321bb28cf25ed6da79d1cf
leegayeong <leegy0419@gmail.com>
2022-01-02 17:52:46 +0900

    로그인 api 접근 권한 설정 #9
    WebSecurityConfigurerAdapter의 configure 메서드 오버라이드

--- src/main/java/com/cocobob/server/config/SecurityConfig.java
+++ src/main/java/com/cocobob/server/config/SecurityConfig.java
@@ -1,9 +1,18 @@
 package com.cocobob.server.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/api/login").permitAll(); //로그인
+    }
 }
+
