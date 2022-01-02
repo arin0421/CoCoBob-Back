deb19472e13e52e009161e746079b18354acb5cf
leegayeong <leegy0419@gmail.com>
2022-01-02 19:02:31 +0900

    AuthContorller 클래스 생성
    로그인 api 추가

---
+++ src/main/java/com/cocobob/server/controller/AuthController.java
@@ -0,0 +1,54 @@
+package com.cocobob.server.controller;
+
+import com.cocobob.server.dto.LoginDTO;
+import com.cocobob.server.dto.TokenDTO;
+import com.cocobob.server.jwt.JwtFilter;
+import com.cocobob.server.jwt.TokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    @Autowired
+    private TokenProvider tokenProvider;
+    @Autowired
+    private AuthenticationManagerBuilder authenticationManagerBuilder;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDTO> authorize(@Valid @RequestBody LoginDTO loginDto, HttpServletResponse response) {
+
+        //loginDTO 정보를 가지고 토큰 생성
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+        //authenticationToken을 이용해서 Authenticate 메서드를 실행하면 loadUserByUsername 메서드 실행
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        //JWT 토큰 생성
+        String jwt = tokenProvider.createToken(authentication);
+
+    //헤더에 토큰 추가
+    HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        return new ResponseEntity<>(new TokenDTO(jwt), httpHeaders, HttpStatus.OK);
+
+
+}
