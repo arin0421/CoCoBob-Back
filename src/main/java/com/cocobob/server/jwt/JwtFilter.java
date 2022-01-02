0ad9beefe87a09f7598f97c043e761ce1893eeec
leegayeong <leegy0419@gmail.com>
2022-01-02 18:22:26 +0900

    doFilter 메서드 수정 #9
    resolveToken을 통해 토큰을 받아와 유효성 검증 후 SecurityContext에 저장

--- src/main/java/com/cocobob/server/jwt/JwtFilter.java
+++ src/main/java/com/cocobob/server/jwt/JwtFilter.java
@@ -28,8 +28,22 @@ public class JwtFilter extends GenericFilterBean {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws  IOException, ServletException{
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String jwt = resolveToken(httpServletRequest); //토큰 정보 저장
+        String requestURI = httpServletRequest.getRequestURI();
 
+        //토큰 유효성 검증
+        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+            Authentication authentication = tokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication); //정상 토큰인 경우 SecurityContext에 저장
+            logger.debug("Security Context에 '{}' 인증 정보를 저장, uri: {}", authentication.getName(), requestURI);
+        } else {
+            logger.debug("유효한 JWT 토큰이 없음, uri: {}", requestURI);
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     //Request header에서 토큰 정보를 꺼내오기
