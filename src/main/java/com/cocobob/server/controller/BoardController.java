28cfaa5710c05e6938445e1089ed5411ad9ae022
Arin0421 <73944423+Arin0421@users.noreply.github.com>
2022-01-02 17:18:19 +0900

    게시물 목록 조회 기능 추가 #4

--- src/main/java/com/cocobob/server/controller/BoardController.java
+++ src/main/java/com/cocobob/server/controller/BoardController.java
@@ -1,8 +1,24 @@
 package com.cocobob.server.controller;
 
+import com.cocobob.server.domain.Board;
+import com.cocobob.server.domain.BoardRequestDto;
+import com.cocobob.server.repository.BoardRepository;
+import com.cocobob.server.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@RestController
 public class BoardController {
 
     private final BoardRepository boardRepository;
+    private final BoardService boardService;
 
     @PostMapping("/api/boards")
     public Board createBoard(@RequestBody BoardRequestDto requestDto){
@@ -17,7 +33,11 @@ public class BoardController {
 
         return board.get();
     }
-    
+
+    @GetMapping("/api/boards")
+    public Page<Board> getBoards(@PageableDefault(size=10,sort="id",direction = Sort.Direction.DESC) Pageable pageable){
+        return boardService.getBoardList(pageable);
+    }
 
 
 }
